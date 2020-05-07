135354ac39d6c39a4716beb45e67e1251d9690a0
moaad <moaadbn3@gmial.com>
2020-05-07 23:35:48 +0200

    removed morgan

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const passport = require('passport')
 const SECRET = process.env.SECRET
 const setUser = require('./middleware/setUser')
 
-app.use(morgan('tiny'))
+// app.use(morgan('tiny'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(passport.initialize())
